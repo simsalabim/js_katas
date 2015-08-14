8f73f3dffd701d3e003b4cbac0dd12cba0cb62ac
Alexander Kaupanin <kaupanin@gmail.com>
2015-08-14 12:15:08 -0500

    Add some validation

--- notation.js
+++ notation.js
@@ -3,6 +3,11 @@ function encode(number) {
   var a, b, c, d, e, counter = 0, alphabet = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h',
                                               'i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
+  var max = Math.pow(alphabet.length, 5) - 1;
+  if (typeof number !== 'number' || (number < 0 || number > max)) {
+    throw Error("Input value must be a number that is greater than 0 and less than " + max);
+  }
+
   for(a = 0; a < alphabet.length; a++) {
     for(b = 0; b < alphabet.length; b++) {
       for(c = 0; c < alphabet.length; c++) {
