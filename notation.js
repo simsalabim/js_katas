0b849b3e90f69bee795792ff3e29e961a8334c48
Alexander Kaupanin <kaupanin@gmail.com>
2015-08-14 10:52:56 -0500

    Notation with base 36; returns 5-digit representation of decimal numbers max to 36^5-1

---
+++ notation.js
@@ -0,0 +1,23 @@
+// Notation with base of 36, maximum encoded number length of 5 digits: 36^5 - 1 = 60466175.
+function encode(number) {
+  var a, b, c, d, e, counter = 0, alphabet = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h',
+                                              'i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+  for(a = 0; a < alphabet.length; a++) {
+    for(b = 0; b < alphabet.length; b++) {
+      for(c = 0; c < alphabet.length; c++) {
+        for(d = 0; d < alphabet.length; d++) {
+          for(e = 0; e < alphabet.length; e++) {
+            if (counter === number) {
+              return (alphabet[a] + alphabet[b] + alphabet[c] + alphabet[d] + alphabet[e]);
+            }
+            counter++;
+          }
+        }
+      }
+    }
+  }
+}
+
+console.log(encode(Math.pow(alphabet.length, 5) - 1));
+// 'zzzzz'
